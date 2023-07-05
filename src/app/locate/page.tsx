f54deaab8394406a1202583f4438135b3dc2ebd9
YukaChoco <sugar.ore.yk@gmail.com>
2023-07-05 23:38:00 +0900

    きたないけどhook使う場所エラーから関数の位置修正

--- src/app/locate/page.tsx
+++ src/app/locate/page.tsx
@@ -43,92 +43,50 @@ type newType = {
   };
 }
 
-export default function location() {
+export default function Location() {
 
-  const searchParams = useSearchParams()
+  const searchParams = useSearchParams();
+  const [nowWeather, setNowWeather] = useState<string>('-');
+
+  const tokyo: City = {
+    latitude: 35.0167,       // 緯度
+    longitude: 135.9667,  // 経度
+  };
+
+  const fetchWeather = async (city: City) => {
+    const queries = new URLSearchParams({
+      latitude: city.latitude.toString(),
+      longitude: city.longitude.toString(),
+      hourly: "weathercode",
+      timezone: "Asia/Tokyo",
+    });
+
+    console.log(queries);
+    const url = `https://api.open-meteo.com/v1/forecast?${queries}`;
+    const response = await fetch(url);
+    console.log(response);
+    const body = await response.json() as newType
+    console.log(body);
+    console.log("query");
+    const date = new Date();
+    console.log(date.getHours());
+
+    return body.hourly.weathercode[date.getHours()];
+  }
+
+  useEffect(() => {
+    fetchWeather(tokyo).then((res) => {
+      const weathercode = res;
+      if (typeof weathercode === 'number') {
+        setNowWeather(convertWeathercodetoText(weathercode));
+      }
+    }).catch(e => console.log(e))
+  }, [])
 
   if (searchParams.has("lat") && searchParams.has("lng")) {
     const lat = searchParams.get("lat");
     const lng = searchParams.get("lng");
-    console.log("lat="+lat+" lng="+lng);
-
-    const [nowWeather, setNowWeather] = useState<string>('-');
-
-    const tokyo: City = {
-      latitude: 35.0167,       // 緯度
-      longitude: 135.9667,  // 経度
-    };
-
-    const fetchWeather = async (city: City) => {
-      const queries = new URLSearchParams({
-        latitude: city.latitude.toString(),
-        longitude: city.longitude.toString(),
-        hourly: "weathercode",
-        timezone: "Asia/Tokyo",
-      });
-      console.log(queries);
-      const url = `https://api.open-meteo.com/v1/forecast?${queries}`;
-      const response = await fetch(url);
-      console.log(response);
-      const body = await response.json() as newType
-      console.log(body);
-      console.log("query");
-
-      const date = new Date();
-      console.log(date.getHours());
-
-      return body.hourly.weathercode[date.getHours()];
-    }
-
-    const convertWeathercodetoText = (weathercode: number) => {
-      switch (weathercode) {
-        case 0:
-          return "晴れ"
-        case 1:
-        case 2:
-        case 3:
-          return "晴れ時々曇り"
-        case 45:
-        case 48:
-          return "曇り"
-        case 51:
-        case 53:
-        case 55:
-        case 56:
-        case 57:
-        case 61:
-        case 63:
-        case 65:
-        case 66:
-        case 67:
-          return "雨"
-        case 71:
-        case 73:
-        case 75:
-        case 77:
-          return "雪"
-        case 80:
-        case 81:
-        case 82:
-          return "雨"
-        case 85:
-        case 86:
-          return "雪"
-        case 95:
-        case 96:
-        case 99:
-          return "雨"
-        default:
-          return "-"
-      }
-    }
-
-    useEffect(() => {
-      fetchWeather(tokyo).then((res) => {
-        const weathercode = res
-        setNowWeather(convertWeathercodetoText(weathercode));
-      }).catch(e => console.log(e))
-    }, [])
+    console.log("lat=" + lat + " lng=" + lng);
 
     return (
       <>
@@ -152,5 +110,49 @@ export default function location() {
         </span>
       </div>
     )
+  }
+}
+
+
+const convertWeathercodetoText = (weathercode: number) => {
+  switch (weathercode) {
+    case 0:
+      return "晴れ"
+    case 1:
+    case 2:
+    case 3:
+      return "晴れ時々曇り"
+    case 45:
+    case 48:
+      return "曇り"
+    case 51:
+    case 53:
+    case 55:
+    case 56:
+    case 57:
+    case 61:
+    case 63:
+    case 65:
+    case 66:
+    case 67:
+      return "雨"
+    case 71:
+    case 73:
+    case 75:
+    case 77:
+      return "雪"
+    case 80:
+    case 81:
+    case 82:
+      return "雨"
+    case 85:
+    case 86:
+      return "雪"
+    case 95:
+    case 96:
+    case 99:
+      return "雨"
+    default:
+      return "-"
   }
 }
