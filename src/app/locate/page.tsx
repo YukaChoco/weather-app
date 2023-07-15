4d41cf827d3f6572bc1412467fe077754ec6eeea
YukaChoco <sugar.ore.yk@gmail.com>
2023-07-15 22:40:14 +0900

    経度緯度パラメータから転機を取得

--- src/app/locate/page.tsx
+++ src/app/locate/page.tsx
@@ -9,23 +9,6 @@ interface City {
   longitude: number,
 }
 
-interface weather {
-  latitude: number;
-  longitude: number;
-  generationtime_ms: number;
-  utc_offset_seconds: number;
-  timezone: string;
-  timezone_abbreviation: string;
-  elevation: number;
-  daily_units: {
-    time: string;
-    weathercode: string;
-  };
-  daily: {
-    time: Date[],
-    weathercode: number[]
-  }
-}
 type newType = {
   latitude: number;
   longitude: number;
@@ -48,48 +31,61 @@ export default function Location() {
 
   const searchParams = useSearchParams();
   const [nowWeather, setNowWeather] = useState<number>(-1);
+  const [location, setLocation] = useState<City>({ latitude: 0, longitude: 0, });
+  const [error, setError] = useState<number>(-1);
 
-  const tokyo: City = {
-    latitude: 35.0167,       // 緯度
-    longitude: 135.9667,  // 経度
-  };
-
-  const fetchWeather = async (city: City) => {
+  const fetchWeather = async () => {
     const queries = new URLSearchParams({
-      latitude: city.latitude.toString(),
-      longitude: city.longitude.toString(),
+      latitude: location.latitude.toString(),
+      longitude: location.longitude.toString(),
       hourly: "weathercode",
       timezone: "Asia/Tokyo",
     });
 
-    console.log(queries);
+    console.log("latitude=" + location.latitude + ",longitude=" + location.longitude + " の天気を取得しました");
+
     const url = `https://api.open-meteo.com/v1/forecast?${queries}`;
     const response = await fetch(url);
-    console.log(response);
-    const body = await response.json() as newType // https://zenn.dev/uzimaru0000/articles/json-type-validation あとで修正
-    //もしくは axios
-    console.log(body);
-    console.log("query");
-    const date = new Date();
-    console.log(date.getHours());
+    const body = await response.json() as newType;
 
-    return body.hourly.weathercode[date.getHours()];
+    const date = new Date();
+
+    const weathercode = body.hourly.weathercode[date.getHours()];
+    setNowWeather(weathercode);
   }
 
   useEffect(() => {
-    fetchWeather(tokyo).then((res) => {
-      const weathercode = res;
-      if (typeof weathercode === 'number') {
-        setNowWeather(weathercode);
+    fetchWeather();
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [location])
+
+  useEffect(() => {
+    const paramLat = searchParams.get("lat");
+    const paramLng = searchParams.get("lng");
+    console.log("lat=" + paramLat + " lng=" + paramLng);
+    if (paramLat === null || paramLng === null) {
+      setError(1);
+      console.log("ERROR:latとlngをパラメータに指定してください");
+    }
+    else {
+      const lat = parseInt(paramLat);
+      const lng = parseInt(paramLng);
+      if (lat < -90 || lat > 90) {
+        setError(2);
+        console.log("ERROR:latは-90から90の範囲で指定してください");
       }
-    }).catch(e => console.log(e))
-  }, [tokyo])
+      else if (lng < -180 || lng > 180) {
+        setError(3);
+        console.log("ERROR:lngは-180から180の範囲で指定してください");
+      }
+      else {
+        setError(0);
+        setLocation({ latitude: lat, longitude: lng });
+      }
+    }
+  }, [searchParams])
 
-  if (searchParams.has("lat") && searchParams.has("lng")) {
-    const lat = searchParams.get("lat");
-    const lng = searchParams.get("lng");
-    console.log("lat=" + lat + " lng=" + lng);
-
+  if (error == 0) {
     return (
       <>
         <Head>
@@ -99,10 +95,18 @@ export default function Location() {
         <main className="flex min-h-screen flex-col items-center justify-between p-24">
           <div className="">
             <ConvertWeathercodetoText weathercode={nowWeather}></ConvertWeathercodetoText>
-            
           </div>
         </main>
       </>
+    )
+  }
+  else if (error == -1) {
+    return (
+      <div className='bg-gray-200 w-screen h-screen text-center flex'>
+        <span className='m-auto w-fit h-fit'>
+          ー Roading ー
+        </span>
+      </div>
     )
   }
   else {
