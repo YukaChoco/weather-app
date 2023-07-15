52d4b10515a0da72be1a32cfd04252fca91f6717
YukaChoco <sugar.ore.yk@gmail.com>
2023-07-15 20:46:37 +0900

    spanの方がいい？

--- src/app/components/ConvertWeathercodetoText.tsx
+++ src/app/components/ConvertWeathercodetoText.tsx
@@ -1,14 +1,14 @@
 export const ConvertWeathercodetoText = ({ weathercode }: { weathercode: number }) => {
   switch (weathercode) {
     case 0:
-      return "晴れ"
+      return <span>晴れ</span>
     case 1:
     case 2:
     case 3:
-      return "晴れ時々曇り"
+      return <span>晴れ時々曇り</span>
     case 45:
     case 48:
-      return "曇り"
+      return <span>曇り</span>
     case 51:
     case 53:
     case 55:
@@ -19,24 +19,24 @@ export const ConvertWeathercodetoText = ({ weathercode }: { weathercode: number 
     case 65:
     case 66:
     case 67:
-      return "雨"
+      return <span>雨</span>
     case 71:
     case 73:
     case 75:
     case 77:
-      return "雪"
+      return <span>雪</span>
     case 80:
     case 81:
     case 82:
-      return "雨"
+      return <span>雨</span>
     case 85:
     case 86:
-      return "雪"
+      return <span>雪</span>
     case 95:
     case 96:
     case 99:
-      return "雨"
+      return <span>雨</span>
     default:
-      return "-"
+      return <span>-</span>
   }
 }
